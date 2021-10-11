1d41be6588adade37e3f16867b85e999c73408f3
Guido Padin <guidopadin17@gmail.com>
2021-10-11 15:40:47 -0300

    avanzando con controllers, rutas, y formularios

--- routes/peliculasRouter.js
+++ routes/peliculasRouter.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 const peliculasController = require("../controllers/peliculasController")
 
-router.get("/detalle", peliculasController.detalle)
+router.get("/detalle/:id", peliculasController.detalle)
 
 router.get("/crear", peliculasController.crear)
 
-router.get("/editar", peliculasController.editar)
+router.get("/editar/:id", peliculasController.editar)
+
+router.post("/guardar", peliculasController.guardar)
+
+router.put("/actualizar/:id", peliculasController.actualizar)
+
+router.delete("/borrar/:id", peliculasController.borrar)
 
 
 module.exports = router;
--- routes/usuariosRouter.js
+++ routes/usuariosRouter.js
@@ -6,8 +6,16 @@ router.get("/ingresar", usuariosController.login)
 
 router.get("/registrarse", usuariosController.registrarse)
 
-router.get("/perfil", usuariosController.perfil)
+router.get("/perfil/:id", usuariosController.perfil)
 
-router.get("/editar", usuariosController.editar)
+router.get("/editar/:id", usuariosController.editar)
+
+router.post("/acceder", usuariosController.acceso)
+
+router.post("/guardar", usuariosController.guardar)
+
+router.put("/actualizar/:id", usuariosController.actualizar)
+
+router.delete("/borrar/:id", usuariosController.borrar)
 
 module.exports = router;
