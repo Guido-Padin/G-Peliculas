9862a0c2c226a03fe8204fde264d8d9ff19cd68d
Guido-Padin <guidopadin17@gmail.com>
2023-02-12 22:43:21 -0300

    anulando ultimos cambios

--- database/config/config.js
+++ database/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     "username": "root",
     "password": null,
     "database": "movies_db",
-    "host": "0.0.0.0",
+    "host": "127.0.0.1",
     "dialect": "mysql",
     "port": 3306
   },
@@ -18,7 +18,7 @@ module.exports = {
     "username": "root",
     "password": null,
     "database": "database_production",
-    "host": "0.0.0.0",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   }
 }
